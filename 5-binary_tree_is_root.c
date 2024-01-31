c08a6134cee0143b326e2e9dddf352219c0cc8f0
paschalugwu <ugwupaschal@gmail.com>
2024-01-31 18:30:16 +0100

    feat(binary_trees): implement binary_tree_is_root function
    Add a new function, binary_tree_is_root, that efficiently checks if a given node in a binary tree is a root. The function returns 1 if the node is a root and 0 otherwise. Tested with provided test cases to ensure correctness and efficiency.

---
+++ 5-binary_tree_is_root.c
@@ -0,0 +1,13 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_root - check if a node is a root.
+ * @node: node to check.
+ * Return: 1 if is, 0 otherwise.
+ */
+int binary_tree_is_root(const binary_tree_t *node)
+{
+	if (node && node->parent == NULL)
+		return (1);
+	return (0);
+}
