3c4fceb13c629d078a1ccac089c63769e14dae54
paschalugwu <ugwupaschal@gmail.com>
2024-02-01 03:39:37 +0100

    feat(binary_trees): implement binary_tree_balance function
    Add a new function, binary_tree_balance, that efficiently measures the balance factor of a binary tree. The function returns 0 if the tree is empty or if the root node is NULL. It utilizes a helper function, binary_tree_height, to calculate the heights of the left and right subtrees. Tested with provided test cases to ensure correctness and efficiency.

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,6 +1,33 @@
 #include "binary_trees.h"
 
 /**
+ * binary_tree_height - measures the height of a binary tree.
+ * @tree: pointer to the root node of the tree to measure the height.
+ *
+ * Return: a size_t value of the hegiht, if tree is NULL return 0.
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t height = 0, right_height = 0, left_height = 0;
+
+	if (tree == NULL)
+		return (0);
+
+	if (tree->left)
+		left_height = binary_tree_height(tree->left);
+	if (tree->right)
+		right_height = binary_tree_height(tree->right);
+
+	if (left_height > right_height)
+		height = left_height;
+	else
+		height = right_height;
+
+	return (height + 1);
+}
+
+
+/**
  * binary_tree_balance - measures the balance factor of a binary tree
  * @tree: pointer to the root node of the tree to measure the height.
  *
