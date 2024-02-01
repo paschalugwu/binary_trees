8fa938b5d210fd580626a362821c3d87d769917e
paschalugwu <ugwupaschal@gmail.com>
2024-02-01 08:44:18 +0100

    feat(binary_trees): implement binary_tree_levelorder function
    Add a new function, binary_tree_levelorder, that efficiently traverses a binary tree using level-order traversal. The function takes a pointer to the root node and a function pointer to be applied to each node. Utilizes a queue data structure for efficient processing. Tested with provided test cases to ensure correctness and efficiency.

--- binary_trees.h
+++ binary_trees.h
@@ -57,6 +57,7 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node);
 /* Advance Tasks */
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
 const binary_tree_t *second);
+void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int));
 
 
 
